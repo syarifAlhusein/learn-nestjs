a00a69abe347243789398be9bd165f9580e82877
syarifAlhusein <muhammadsyarifalhusein@gmail.com>
2026-09-10 14:58:40 +0700

    feat: task controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
+  controllers: [TasksController],
 })
-export class AppModule {}
+
+export class AppModule { }
